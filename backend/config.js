66a40971cf252268d31f7cc349a018932760cc3d
smol-kitty <155082547+smol-kitty@users.noreply.github.com>
2024-07-13 12:24:01 +0530

    Add files via upload

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  "mongodb+srv://<username>:<password>@books-store-mern-ash.enuljlv.mongodb.net/?retryWrites=true&w=majority&appName=Books-Store-MERN-ash";
---
+++ backend/index.js
@@ -0,0 +1,39 @@
+import express, { request, response } from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose, { connect } from "mongoose";
+import { Book } from "./models/bookModel.js";
+import booksRoute from "./routes/bookRoute.js";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+/*app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
+*/
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("Welcome to MERN Stack Tutorial");
+});
+
+app.use("/books", booksRoute);
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App connected to database");
+    app.listen(PORT, () => {
+      console.log(`App is listening on port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
